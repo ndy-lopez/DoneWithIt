1471256318f0f18a84eba9a5cb40f5267f08a835
Andy Loredo <ndy.loredo@gmail.com>
2023-10-02 12:15:14 -0400

    Add image and tested UI in Expo

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { StyleSheet, Text, View, Image, SafeAreaView } from "react-native";
 
 export default function App() {
+  const handlePress = () => console.log("Text pressed");
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello React Native</Text>
+      <Text numberOfLines={1} onPress={handlePress}>
+        Hello React Native
+      </Text>
+      <Image
+        source={{ uri: "https://picsum.photos/300/600" }}
+        width={200}
+        height={300}
+      />
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -13,6 +22,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "dodgerblue",
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
