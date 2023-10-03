bc6e7acf2b245567d198a14a42f90a787be59103
Andy Loredo <ndy.loredo@gmail.com>
2023-10-03 11:11:27 -0400

    Test button alert and prompt

--- App.js
+++ App.js
@@ -1,33 +1,21 @@
 import { StatusBar } from "expo-status-bar";
-import {
-  StyleSheet,
-  Text,
-  TouchableWithoutFeedback,
-  View,
-  Image,
-  SafeAreaView,
-} from "react-native";
+import { StyleSheet, SafeAreaView, Alert, Button } from "react-native";
 
 export default function App() {
   const handlePress = () => console.log("Text pressed");
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text numberOfLines={1} onPress={handlePress}>
-        Hello React Native
-      </Text>
-      <TouchableWithoutFeedback onPress={() => console.log("Image tapped")}>
-        <Image
-          source={{
-            blurRadius: 10,
-            fadeDuration: 1000,
-            width: 200,
-            height: 300,
-            uri: "https://picsum.photos/200/300",
-          }}
-        />
-      </TouchableWithoutFeedback>
-      <StatusBar style="auto" />
+      <Button
+        color="orange"
+        title="Click Me"
+        onPress={() =>
+          Alert.alert("My title", "My message", [
+            { text: "Yes", onPress: () => console.log("Yes") },
+            { text: "No", onPress: () => console.log("No") },
+          ])
+        }
+      />
     </SafeAreaView>
   );
 }
